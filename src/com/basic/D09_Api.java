d1d05bf81ec9a054f871302cfe096dfc2c39a730
liuyuanyuan <1445179059@qq.com>
2020-07-01 22:58:28 +0800

    java-Scanner和Random学习

--- src/com/basic/D09_Api.java
+++ src/com/basic/D09_Api.java
@@ -4,8 +4,6 @@ import java.util.Random;
 import java.util.Scanner;    //alt+enter 导包
 
 
-
-
 public class D09_Api {
     public static void main(String[] args) {
         random();
@@ -13,7 +11,7 @@ public class D09_Api {
     }
 
     private static void random() {
-        Random r = new Random();  ////产生0-10不包含10随机整数
+        Random r = new Random();  //产生0-10(不包含10)随机整数
         for (int i = 0; i < 30; i++) {
             System.out.println(i + ":" + r.nextInt(10));
         }
