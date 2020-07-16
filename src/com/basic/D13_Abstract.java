584c9ff7e4d2ab94da7e8c9c1875e2be1e158abf
liuyuanyuan <1445179059@qq.com>
2020-07-16 23:02:41 +0800

    类的继承结束

---
+++ src/com/basic/D13_Abstract.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class D13_Abstract {
+    public static void main(String[] args) {
+        /*
+         * ³éÏóÀà
+
+         * */
+    }
+}
--- src/com/basic/D13_extend/D01_extends.java
+++ src/com/basic/D13_extend/D01_extends.java
@@ -3,6 +3,7 @@ package com.basic.D13_extend;
 public class D01_extends {
     public static void main(String[] args) {
         D03_Son s = new D03_Son();
-        s.show();
+//        s.show();
+        s.method(); //ÎÒÊÇ¶ù×Ó
     }
 }
