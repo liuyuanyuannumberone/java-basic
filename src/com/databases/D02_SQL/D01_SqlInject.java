8248ad4bb78fb40f8cc28ab8b7433cc25023785c
liuyuanyuan <1445179059@qq.com>
2020-08-09 22:35:08 +0800

    20200809-数据库连接池

---
+++ src/com/databases/D02_SQL/D01_SqlInject.java
@@ -0,0 +1,35 @@
+package com.databases.D02_SQL;
+
+import com.databases.D01_JDBC.MysqlUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class D01_SqlInject {
+  public static void main(String[] args) throws SQLException {
+    //sql注入:当用户的数据中包含一些SQL的符号，如果我们不加判断就直接封装到sql语句中，从而会改变我们整个sql
+    //的含义，这种情况就叫做SQL注入.
+    /*
+    Connection con = MysqlUtils.getConnection();
+    Statement stm = con.createStatement();
+    stm.close();
+    con.close();
+    */
+
+    //解决办法：ps内部转义避免了sql注入问题,w完全代替Statement
+    String name = "lyy";
+    String passwd = "passok";
+    Connection con = MysqlUtils.getConnection();
+    String sql = "select * from  user where userName=?and userPwd=?";
+    PreparedStatement ps = con.prepareStatement(sql);
+    ps.setString(1, name);
+    ps.setString(2, passwd);
+    ps.executeQuery();
+
+    ps.close();
+    con.close();
+
+  }
+}
